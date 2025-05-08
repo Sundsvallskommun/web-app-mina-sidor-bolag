8abff1c66ae996ebb05a792e42b1f4e4a71e68a1
ali18rin <alice.ringstrom@sundsvall.se>
2025-05-08 12:00:36 +0200

    feature: login design changes

--- frontend/src/layouts/entry-layout.component.tsx
+++ frontend/src/layouts/entry-layout.component.tsx
@@ -1,6 +1,10 @@
-import { Logo, cx } from '@sk-web-gui/react';
-import { appName } from '../utils/app-name';
+'use client';
+
+import { cx } from '@sk-web-gui/react';
+import { appName } from '@utils/app-name';
 import EmptyLayout from './empty-layout.component';
+import Image from 'next/image';
+import React from 'react';
 
 export const EntryLayout: React.FC<{
   title: string;
@@ -11,21 +15,30 @@ export const EntryLayout: React.FC<{
   return (
     <EmptyLayout title={`${appName()} - ${title}`}>
       <div className="relative">
-        <div className="absolute w-full bg-vattjom-background-200">
+        <div className="absolute w-full bg-error-background-100">
           <div className="h-[26.4rem] max-w-[80rem] mx-auto relative overflow-hidden">
-            <div className="hidden lg:block -mt-[4rem] -ml-34 absolute w-[36rem]">
-              <Logo variant="symbol" className="text-vattjom-surface-accent" />
-            </div>
+            <div className="hidden lg:block -mt-[4rem] -ml-34 absolute w-[36rem]"></div>
           </div>
         </div>
-        <div
-          className={cx('relative flex flex-col items-center justify-center px-20 py-40 lg:py-80 lg:px-40', className)}
-        >
-          <Logo
-            variant="logo"
-            className={cx(`text-black w-[16.4rem] h-[5.6rem] lg:h-[7.2rem] mb-32 lg:mb-48`, logoClasses)}
-          />
-          {children}
+        <div className={cx('relative items-center justify-center px-20 py-40 lg:py-80 lg:px-40', className)}>
+          <div className="flex justify-self-center gap-24">
+            <Image
+              className={cx(`text-black w-[7.7rem] h-[3.6rem] lg:h-[5rem] lg:w-[10rem] mb-32 lg:mb-48`, logoClasses)}
+              src="/sundsvall-elnat.webp"
+              alt={'Sundsvall elnäts logotyp'}
+              width={100}
+              height={50}
+            />
+            <Image
+              className={cx(`text-black w-[7.7rem] h-[3.6rem] lg:h-[5rem] lg:w-[10rem] mb-32 lg:mb-48`, logoClasses)}
+              src="/sundsvall-energi.png"
+              alt={'Sundsvall energis logotyp'}
+              width={100}
+              height={50}
+            />
+          </div>
+
+          <div className="justify-self-center">{children}</div>
         </div>
       </div>
     </EmptyLayout>
