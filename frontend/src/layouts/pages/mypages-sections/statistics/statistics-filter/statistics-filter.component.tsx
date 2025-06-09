7a004a11ed184ee6060a85a63e281318efa6a6c7
ali18rin <alice.ringstrom@sundsvall.se>
2025-06-09 17:52:11 +0200

    feat: added spinner when fetching user data

--- frontend/src/layouts/pages/mypages-sections/statistics/statistics-filter/statistics-filter.component.tsx
+++ frontend/src/layouts/pages/mypages-sections/statistics/statistics-filter/statistics-filter.component.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { DatePicker, FormLabel, Input, Select } from '@sk-web-gui/react';
+import { DatePicker, FormLabel, Input, Select, Spinner } from '@sk-web-gui/react';
 import { useApi } from '@services/api-service';
 import { User } from '@interfaces/user';
 import { useFormContext } from 'react-hook-form';
@@ -14,7 +14,7 @@ export const StatisticsFilter = () => {
   const [facilities, setFacilities] = useState<InstalledBaseItem[]>();
   const { address, category } = watch();
 
-  const { data: user } = useApi<User>({
+  const { data: user, isFetching } = useApi<User>({
     method: 'get',
     url: '/me',
     queryKey: ['user'],
@@ -54,67 +54,67 @@ export const StatisticsFilter = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [category]);
 
-  return (
-    user && (
-      <section className="lg:flex lg:justify-between block gap-24 lg:pt-0 pt-24">
-        <div className="block w-full">
-          <Input {...register('facilityId')} hidden />
-          <FormLabel>Adress</FormLabel>
+  return user && !isFetching ? (
+    <section className="lg:flex lg:justify-between block gap-24 lg:pt-0 pt-24">
+      <div className="block w-full">
+        <Input {...register('facilityId')} hidden />
+        <FormLabel>Adress</FormLabel>
 
-          <Select {...register('address')} className="w-full mt-8">
-            {user.addresses.map((address) => (
-              <Select.Option key={address.address}>{address.address ? address.address : 'Okänd adress'}</Select.Option>
-            ))}
-          </Select>
-        </div>
+        <Select {...register('address')} className="w-full mt-8">
+          {user.addresses.map((address) => (
+            <Select.Option key={address.address}>{address.address ? address.address : 'Okänd adress'}</Select.Option>
+          ))}
+        </Select>
+      </div>
 
-        <div className="block w-full lg:pt-0 pt-16">
-          <FormLabel>Kategori</FormLabel>
-          <Select {...register('category')} className="w-full mt-8">
-            {facilities?.map((facility) => {
-              return (
-                <Select.Option key={facility.facilityId + '-' + facility.type} value={facility.type}>
-                  {facility.type}
-                </Select.Option>
-              );
-            })}
-          </Select>
-        </div>
+      <div className="block w-full lg:pt-0 pt-16">
+        <FormLabel>Kategori</FormLabel>
+        <Select {...register('category')} className="w-full mt-8">
+          {facilities?.map((facility) => {
+            return (
+              <Select.Option key={facility.facilityId + '-' + facility.type} value={facility.type}>
+                {facility.type}
+              </Select.Option>
+            );
+          })}
+        </Select>
+      </div>
 
-        <div className="block w-full lg:pt-0 pt-16">
-          <FormLabel>Från</FormLabel>
-          <DatePicker
-            {...register('fromDate')}
-            className="w-full mt-8"
-            defaultValue={dayjs().startOf('year').format('YYYY-MM-DD HH:mm')}
-            type="datetime-local"
-            max={dayjs().format('YYYY-MM-DD HH:mm')}
-          />
-        </div>
+      <div className="block w-full lg:pt-0 pt-16">
+        <FormLabel>Från</FormLabel>
+        <DatePicker
+          {...register('fromDate')}
+          className="w-full mt-8"
+          defaultValue={dayjs().startOf('year').format('YYYY-MM-DD HH:mm')}
+          type="datetime-local"
+          max={dayjs().format('YYYY-MM-DD HH:mm')}
+        />
+      </div>
 
-        <div className="block w-full lg:pt-0 pt-16">
-          <FormLabel>Till</FormLabel>
-          <DatePicker
-            {...register('toDate')}
-            defaultValue={dayjs().format('YYYY-MM-DD HH:mm')}
-            className="w-full mt-8"
-            type="datetime-local"
-            max={dayjs().format('YYYY-MM-DD HH:mm')}
-          />
-        </div>
+      <div className="block w-full lg:pt-0 pt-16">
+        <FormLabel>Till</FormLabel>
+        <DatePicker
+          {...register('toDate')}
+          defaultValue={dayjs().format('YYYY-MM-DD HH:mm')}
+          className="w-full mt-8"
+          type="datetime-local"
+          max={dayjs().format('YYYY-MM-DD HH:mm')}
+        />
+      </div>
 
-        <div className="block w-full lg:pt-0 pt-16">
-          <FormLabel>Jämför med år</FormLabel>
-          <Select {...register('year')} className="w-full mt-8">
-            <Select.Option key={0} value="">
-              Välj år
-            </Select.Option>
-            {generateYearsBetween().map((year) => (
-              <Select.Option key={year}>{year}</Select.Option>
-            ))}
-          </Select>
-        </div>
-      </section>
-    )
+      <div className="block w-full lg:pt-0 pt-16">
+        <FormLabel>Jämför med år</FormLabel>
+        <Select {...register('year')} className="w-full mt-8">
+          <Select.Option key={0} value="">
+            Välj år
+          </Select.Option>
+          {generateYearsBetween().map((year) => (
+            <Select.Option key={year}>{year}</Select.Option>
+          ))}
+        </Select>
+      </div>
+    </section>
+  ) : (
+    <Spinner className="mx-auto" />
   );
 };
