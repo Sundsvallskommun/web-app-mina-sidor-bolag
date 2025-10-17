a67828e2497660feb764ddd344d2424451d3e3d7
aliceringstromsk <alice.ringstrom@sundsvall.se>
2025-10-17 13:17:37 +0200

    test: update

--- frontend/cypress/fixtures/getMyRelations.ts
+++ frontend/cypress/fixtures/getMyRelations.ts
@@ -1,20 +1,22 @@
 import { ApiResponse } from '@services/api-service';
 import { CustomerRelation } from '@data-contracts/customer/data-contracts';
 
-export const getMyRelations: () => ApiResponse<CustomerRelation[]> = () => ({
+export const getMyRelations: ApiResponse<CustomerRelation[]> = {
   data: [
     {
       customerNumber: '1',
       organizationNumber: '5564786647',
       organizationName: 'Sundsvall Energi',
       active: true,
+      moveInDate: '2025-01-01',
     },
     {
       customerNumber: '1',
       organizationNumber: '5565027223',
       organizationName: 'Sundsvall Elnät',
       active: true,
+      moveInDate: '2025-01-01',
     },
   ],
   message: 'success',
-});
+};
