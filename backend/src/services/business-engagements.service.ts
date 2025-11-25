9b70d56920a10935314e69e5621b575df7a8d057
SUKMTobNorx <tobias.nordin@sundsvall.se>
2025-11-25 21:11:10 +0100

    feat: adds engagements from mandate

--- backend/src/services/business-engagements.service.ts
+++ backend/src/services/business-engagements.service.ts
@@ -1,17 +1,19 @@
-import { ENVIRONMENT, MOCK_ORGANIZATION_NAME, MOCK_ORGANIZATION_NUMBER, MUNICIPALITY_ID } from '@/config';
+import { ENVIRONMENT, MOCK_ORGANIZATION_NAME, MOCK_ORGANIZATION_NUMBER, MUNICIPALITY_ID, NAMESPACE } from '@/config';
 import { getApiBase } from '@/config/api-config';
 import { LegalEntity2, PersonEngagement } from '@/data-contracts/legalentity/data-contracts';
 import { HttpException } from '@/exceptions/HttpException';
 import { BusinessInformation } from '@/interfaces/business-engagement';
 import { User } from '@/interfaces/users.interface';
 import ApiService from './api.service';
+import { Mandates } from '@/data-contracts/myrepresentatives/data-contracts';
 
-export const getBusinessEngagements = async (personalNumber: string): Promise<PersonEngagement[]> => {
-  if (!personalNumber) {
+export const getBusinessEngagements = async (user: User): Promise<PersonEngagement[]> => {
+  if (!user.personNumber) {
     throw new Error('Bad Request: personalNumber is required');
   }
   const apiBase = getApiBase('legalentity');
-  const url = `${apiBase}/${MUNICIPALITY_ID}/engagements/person/${personalNumber}`;
+  const apiService = new ApiService();
+  const url = `${apiBase}/${MUNICIPALITY_ID}/engagements/person/${user.personNumber}`;
 
   let res: { data: PersonEngagement[] };
   if (ENVIRONMENT === 'TEST' && MOCK_ORGANIZATION_NAME && MOCK_ORGANIZATION_NUMBER) {
@@ -35,9 +37,11 @@ export const getBusinessEngagements = async (personalNumber: string): Promise<Pe
       ],
     };
   } else {
-    const apiService = new ApiService();
     res = await apiService.get<PersonEngagement[]>({ url }, { username: 'unknown' });
   }
+
+  await addEngagementFromMandate(user, apiService, apiBase, res);
+
   return res.data ?? [];
 };
 
@@ -70,4 +74,34 @@ export const getBusinessInformation = async (
   return { address, partyId: guid.data };
 };
 
-export default getBusinessEngagements;
+const addEngagementFromMandate = async (
+  user: User,
+  apiService: ApiService,
+  apiBase: string,
+  res: {
+    data: PersonEngagement[];
+  },
+) => {
+  const mandateApiUrl = `${getApiBase('myrepresentatives')}/${MUNICIPALITY_ID}/${NAMESPACE}/mandates`;
+  const mandateParams = {
+    granteePartyId: user.partyId,
+  };
+
+  const mandateRes = await apiService.get<Mandates>({ url: mandateApiUrl, params: mandateParams }, user);
+
+  if (mandateRes && mandateRes.data.mandateDetailsList.length > 0) {
+    await Promise.all(
+      mandateRes.data.mandateDetailsList.map(async m => {
+        const url = `${apiBase}/${MUNICIPALITY_ID}/${m.grantorDetails.grantorPartyId}`;
+        const companyByGrantor = await apiService.get<LegalEntity2>({ url }, user);
+
+        res.data.push({
+          organizationNumber: companyByGrantor.data.organizationNumber,
+          name: companyByGrantor.data.name,
+          isAuthorizedSignatory: true,
+          isSoleTrader: null,
+        });
+      }),
+    );
+  }
+};
