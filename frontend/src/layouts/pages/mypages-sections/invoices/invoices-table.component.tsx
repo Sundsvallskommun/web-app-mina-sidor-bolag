edbcfb8c9d61c73a25272a27f94f46195ba1fa5d
Alice Ringström <alice.ringstrom@sundsvall.se>
2025-11-25 18:01:36 +0100

    fix: invoice contractor

--- frontend/src/layouts/pages/mypages-sections/invoices/invoices-table.component.tsx
+++ frontend/src/layouts/pages/mypages-sections/invoices/invoices-table.component.tsx
@@ -26,7 +26,7 @@ export const InvoicesTable = ({ pageSize, facilityIds, emptyComponent, onlyPendi
   const [activePage, setActivePage] = useState(1);
   const [rows, setRows] = useState<IInvoice[]>([]);
   const totalCount = useRef<number>(0);
-  const { t } = useTranslation(['common', 'invoice']);
+  const { t } = useTranslation(['common', 'invoice', 'organization']);
 
   const previousActivePage = useRef<number>(-1);
   const previousRepresentingMode = useRef<RepresentingMode | undefined>(undefined);
@@ -77,10 +77,10 @@ export const InvoicesTable = ({ pageSize, facilityIds, emptyComponent, onlyPendi
   const getOrganizationName = useMemo(
     () =>
       (organizationNumber: string): string => {
-        return (
-          userData?.relations.find((relation) => relation.organizationNumber === organizationNumber)
-            ?.organizationName ?? t('common:unknown')
-        );
+        return organizationNumber === '5565027223'
+          ? t('organization:5564786647.name')
+          : (userData?.relations.find((relation) => relation.organizationNumber === organizationNumber)
+              ?.organizationName ?? t('common:unknown'));
       },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [userData]
