f3c6ae7da784bcdee886b94c353d5d092901c0ea
SUKMTobNorx <tobias.nordin@sundsvall.se>
2025-12-03 14:00:44 +0100

    feat: adds controller for fetching customer code

--- backend/src/interfaces/bfus.interface.ts
+++ backend/src/interfaces/bfus.interface.ts
@@ -1,12 +1,10 @@
-import { PermissionStatusCategory } from '@/utils/bfus-permission-status-code-helpers';
-
 export interface BFUSCustomerResponse {
   Header: BFUSCustomerHeader;
   Content: BFUSCustomerContent;
 }
 
 export interface BFUSCustomerHeader {
-  ErrorInformation: string | null;
+  ErrorInformation: any | null;
   ObjectVersion: number;
   Success: boolean;
   PerformanceTime: string;
@@ -67,76 +65,6 @@ export interface BFUSCustomer {
   Email3: string | null;
   BusinessUnitId: number;
   BusinessUnitExternalReferenceCode: number;
-  ContactPersons: string[] | null;
+  ContactPersons: any | null;
   ObjectVersion: number;
-}
-
-export interface BFUSEligablePartyResponse {
-  Header: BFUSEligablePartyHeader;
-  Content: BFUSEligablePartyContent;
-}
-
-export interface BFUSEligablePartyHeader {
-  ErrorInformation: string | null;
-  ObjectVersion: number;
-  Success: boolean;
-  PerformanceTime: string;
-  InParameters: string[];
-}
-
-export interface BFUSEligablePartyContent {
-  EligablePartyParts: BFUSEligablePartyPart[];
-}
-
-export interface BFUSEligablePartyPart {
-  EnergyServiceParty: string;
-  LastDayToApprove: string;
-  ContractReference: string;
-  UserHandledTime: string | null;
-  PermissionId: string;
-  StartDay: string;
-  EndDay: string | null;
-  UserRevokedContractTime: string | null;
-  EndReason: BFUSEndReason;
-  RequestPurpose: string;
-  ContractInUse: boolean;
-  ContractId: number;
-  Status: string;
-  EligablePartyPermissionId: number;
-  CustomerId: number;
-  ServiceIdentifier: string;
-  UsePlaceAddress: string;
-  EligablePartyId: string;
-  StatusCode: BFUSStatusCode;
-  IsProduction: boolean;
-  ObjectVersion: number;
-  StatusCategory: PermissionStatusCategory;
-}
-
-export interface BFUSEligablePartyPermissionResponse {
-  Header: {
-    ErrorInformation: string | null;
-    ObjectVersion: number;
-    Success: boolean;
-    PerformanceTime: string;
-    InParameters: string | null;
-  };
-  Content: {
-    EligablePartyId: string;
-    Action: string;
-    PermissionRequestExecuted: boolean;
-  };
-}
-
-export enum BFUSStatusCode {
-  New = 0,
-  Active = 1,
-  Denied = 2,
-  Ended = 3,
-}
-
-export enum BFUSEndReason {
-  Revoked = 'Återkallat',
-  Expired = 'Upphört',
-  Dismissed = 'Uppsagt',
 }
--- backend/src/responses/bfus.response.ts
+++ backend/src/responses/bfus.response.ts
@@ -1,17 +1,8 @@
-import { BFUSEligablePartyPart } from '@/interfaces/bfus.interface';
-import { IsArray, IsNumber, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class BFUSApiResponse {
   @IsString()
   message: string;
-  @IsNumber()
-  @IsArray()
-  customerIds: number[];
-}
-
-export class BFUSEligablePartyApiResponse {
   @IsString()
-  message: string;
-  @IsArray()
-  eligablePartyParts: BFUSEligablePartyPart[];
+  customerCode: string;
 }
