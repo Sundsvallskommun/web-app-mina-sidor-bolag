38b131826f544a1bf2f9cd63d976f0574d9f0b3a
Alice Ringström <alice.ringstrom@sundsvall.se>
2025-12-04 14:36:36 +0100

    wip: handling of multiple customer numbers

--- backend/src/responses/bfus.response.ts
+++ backend/src/responses/bfus.response.ts
@@ -1,8 +1,8 @@
-import { IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class BFUSApiResponse {
   @IsString()
   message: string;
-  @IsString()
-  customerCode: string;
+  @IsNumber()
+  customerId: number;
 }
