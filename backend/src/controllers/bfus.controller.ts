b59c6056f18b51dee6a3e046f101b81c5e128284
SUKMTobNorx <tobias.nordin@sundsvall.se>
2025-12-08 10:51:27 +0100

    feat: adds get request for eligable party

--- backend/src/controllers/bfus.controller.ts
+++ backend/src/controllers/bfus.controller.ts
@@ -1,14 +1,15 @@
 import { BFUS_API_KEY, BFUS_BASE_URL, BFUS_EXTERNAL_ID } from '@/config';
 import { HttpException } from '@/exceptions/HttpException';
 import { RequestWithUser } from '@/interfaces/auth.interface';
-import { BFUSCustomerResponse } from '@/interfaces/bfus.interface';
+import { BFUSCustomerResponse, BFUSEligablePartyResponse } from '@/interfaces/bfus.interface';
 import authMiddleware from '@/middlewares/auth.middleware';
-import { BFUSApiResponse } from '@/responses/bfus.response';
+import { BFUSApiResponse, BFUSEligablePartyApiResponse } from '@/responses/bfus.response';
 import { logger } from '@/utils/logger';
 import axios, { AxiosResponse } from 'axios';
 import { Response } from 'express';
-import { Controller, Get, HttpError, Req, Res, UseBefore } from 'routing-controllers';
+import { Controller, Get, HttpError, QueryParam, Req, Res, UseBefore } from 'routing-controllers';
 import { OpenAPI } from 'routing-controllers-openapi';
+import _ from 'lodash';
 
 @Controller()
 export class BFUSController {
@@ -47,6 +48,39 @@ export class BFUSController {
       });
     } catch (error: any) {
       logger.error('Unexpected error in BFUS Customer', error.message);
+      throw new HttpError(500, 'Unexpected server error');
+    }
+  }
+
+  @Get('/eligable-party-permissions')
+  @OpenAPI({ summary: 'Returns a list with eligable party permissions' })
+  @UseBefore(authMiddleware)
+  async GetEligablePartyPermissions(
+    @QueryParam('customerId') customerId: string,
+    @Res() res: Response<BFUSEligablePartyApiResponse>,
+  ): Promise<Response<BFUSEligablePartyApiResponse>> {
+    if (!customerId) {
+      throw new HttpException(400, 'No customer id found');
+    }
+
+    const customerIdNumber = Number(customerId);
+
+    if (Number.isNaN(customerIdNumber)) {
+      throw new HttpException(400, 'Customer id must be a number');
+    }
+
+    try {
+      const url = `${BFUS_BASE_URL}/EP/EligableParty/EligablePartyPermissions/${BFUS_EXTERNAL_ID}/${customerId}`;
+      const result = await axios.get<BFUSEligablePartyResponse>(url, {
+        headers: { Authorization: BFUS_API_KEY },
+      });
+
+      return res.send({
+        message: _.isEmpty(result.data.Content) ? 'no eligable party parts available' : 'success',
+        eligablePartyParts: result.data.Content.EligablePartyParts,
+      });
+    } catch (error: any) {
+      logger.error('Unexpected error in BFUS eligable party permissions', error.message);
       throw new HttpError(500, 'Unexpected server error');
     }
   }
