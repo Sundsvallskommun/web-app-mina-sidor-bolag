757230a562a1f84757d8d21f2deb00be2fa93cb6
SUKMTobNorx <tobias.nordin@sundsvall.se>
2025-12-11 07:08:59 +0100

    fix: corrects dto's

--- backend/src/dtos/permission-request.dto.ts
+++ backend/src/dtos/permission-request.dto.ts
@@ -1,11 +1,24 @@
+import { IsString, IsOptional, IsArray, IsNumber } from 'class-validator';
+
 export class PermissionHeaderDto {
+  @IsString()
   ExternalId: string;
+
+  @IsString()
   Operation: 'grant' | 'deny' | 'revoke';
 }
 
 export class PermissionRequestDto {
+  @IsString()
   EligablePartyId: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsNumber({}, { each: true })
   ContractIdList?: number[];
+
+  @IsOptional()
+  @IsNumber()
   CustomerId?: number;
 }
 
