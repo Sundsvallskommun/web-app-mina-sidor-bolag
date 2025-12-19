f9747d2d098315b0d709f74e8ceaaa092b515bc6
Alice Ringström <alice.ringstrom@sundsvall.se>
2025-12-19 11:09:44 +0100

    fix: use hook

--- frontend/src/layouts/pages/mypages-sections/eligibility/eligibility.component.tsx
+++ frontend/src/layouts/pages/mypages-sections/eligibility/eligibility.component.tsx
@@ -3,20 +3,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { NewPermissions } from '@layouts/pages/mypages-sections/eligibility/new-permissions/new-permissions.component';
-import { useApi } from '@services/api-service';
 import { Spinner } from '@sk-web-gui/react';
-import { BFUSCustomerIdsApiResponse } from '@interfaces/eligibility';
 import CurrentAndClosedEligibilityPermissions from './current-and-closed-eligibility-permissions';
+import { useGetCustomerId } from '@services/eligibility-service';
 
 export default function Eligibility() {
   const { t } = useTranslation('eligibility');
 
-  const { data: customerIds, isFetching } = useApi<BFUSCustomerIdsApiResponse['data']>({
-    url: '/bfus/eligable-party-customer-id',
-    queryKey: ['bfus-customer-ids'],
-    method: 'get',
-    dataHandler: (data) => data,
-  });
+  const { data: customerIds, isFetching } = useGetCustomerId();
 
   return (
     <div>
@@ -27,11 +21,10 @@ export default function Eligibility() {
         <Spinner />
       ) : customerIds ? (
         <div className="pt-40">
-          <NewPermissions customerIds={customerIds.customerIds} />
+          <NewPermissions customerIds={customerIds} />
+          <CurrentAndClosedEligibilityPermissions customerIds={customerIds} />
         </div>
       ) : null}
-
-      <CurrentAndClosedEligibilityPermissions customerIds={customerIds} />
     </div>
   );
 }
