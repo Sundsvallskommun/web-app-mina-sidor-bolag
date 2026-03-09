66c0cb17f03b43e30d36b6a45b43a2220a9d1040
aliceringstromsk <alice.ringstrom@sundsvall.se>
2026-03-09 17:35:30 +0100

    feat: handle whitelisted mandates

--- frontend/cypress/e2e/fullmakter.cy.ts
+++ frontend/cypress/e2e/fullmakter.cy.ts
@@ -16,20 +16,19 @@ describe('Profil och inställningar', () => {
     cy.visit('/foretag/profil');
   });
 
-  it('users with signing rights can create mandates', () => {
+  it('users with signing rights can access and create mandates', () => {
     setIntercepts(RepresentingMode.BUSINESS, 0);
     cy.get('[data-cy="mandate-disclosure"]').click();
     cy.get('[data-cy="create-mandate-button"]').should('exist');
   });
 
-  it('users without signing rights or whitelisting cannot create mandates', () => {
+  it('users without signing rights or whitelisting cannot access mandates', () => {
     setIntercepts(RepresentingMode.BUSINESS, 1);
     cy.visit('/foretag/profil');
-    cy.get('[data-cy="mandate-disclosure"]').click();
-    cy.get('[data-cy="create-mandate-button"]').should('not.exist');
+    cy.get('[data-cy="mandate-disclosure"]').should('not.exist');
   });
 
-  it('whitelisted users can create mandates', () => {
+  it('whitelisted users can access and create mandates', () => {
     setIntercepts(RepresentingMode.BUSINESS, 2);
     cy.get('[data-cy="mandate-disclosure"]').click();
     cy.get('[data-cy="create-mandate-button"]').should('exist');
