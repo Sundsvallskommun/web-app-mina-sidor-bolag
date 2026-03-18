e71864385bf0cbb973e6bdf56ee4f006d1f8a05c
AbedNaqeebSundsvall <abed.naqeeb@sundsvall.se>
2026-03-18 17:13:35 +0100

    fix: match facilityId type to backend

--- frontend/src/interfaces/measurement-data.ts
+++ frontend/src/interfaces/measurement-data.ts
@@ -23,7 +23,7 @@ export enum Category {
 
 export interface Data {
   category?: Category;
-  facilityId?: string;
+  facilityId?: string[];
   aggregateOn?: Aggregation;
   fromDate?: string;
   toDate?: string;
