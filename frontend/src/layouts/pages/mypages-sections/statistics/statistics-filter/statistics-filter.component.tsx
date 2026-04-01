64ca9462197123f53bf965c0c03ae0b79b8a5552
AbedNaqeebSundsvall <abed.naqeeb@sundsvall.se>
2026-04-01 17:32:37 +0200

    fix: change timeInterval to 60min when change mode from day.

--- frontend/src/layouts/pages/mypages-sections/statistics/statistics-filter/statistics-filter.component.tsx
+++ frontend/src/layouts/pages/mypages-sections/statistics/statistics-filter/statistics-filter.component.tsx
@@ -90,6 +90,12 @@ export const StatisticsFilter = (props: StatisticsFilterProps) => {
   }, []);
 
   useEffect(() => {
+    if (mode !== 'day') {
+      setValue('isHourQuarter', false);
+    }
+  }, [mode, setValue]);
+
+  useEffect(() => {
     const setDate = (by: 'year' | 'month' | 'day', _date?: Dayjs) => {
       const date =
         _date ??
