75914873e928d7575067072be4bf08fd5631e045
Tobias Nordin <tobias.nordin@sundsvall.se>
2026-05-18 12:22:56 +0200

    fix: fixes search state | updates translation

--- frontend/src/layouts/pages/mypages-sections/agreements.component.tsx
+++ frontend/src/layouts/pages/mypages-sections/agreements.component.tsx
@@ -2,7 +2,7 @@
 
 import { Pagination, SearchField, Select, Spinner } from '@sk-web-gui/react';
 import { AgreementListItem } from '@layouts/pages/mypages-sections/agreements/agreement-list-item/agreement-list-item.component';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useApi } from '@services/api-service';
 import { getCategoryAsNumber, pagedAgreementsWithMetaHandler } from '@services/agreement-service';
 import { AgreementData, RefinedAgreement } from '@interfaces/agreement';
@@ -28,33 +28,32 @@ export default function PagedAgreements() {
   const agreements = response?.agreements;
   const meta = response?._meta;
 
-  const [data, setData] = useState<AgreementData | undefined>(agreements);
   const [term, setTerm] = useState<string>('');
   const { t } = useTranslation(['common', 'agreement']);
 
   useEffect(() => {
-    setData(agreements);
     setPages(meta?.totalPages ?? 1);
-  }, [agreements, meta]);
+  }, [meta]);
+
+  const data = useMemo<AgreementData | undefined>(() => {
+    if (!agreements) return undefined;
+    if (term.length <= 1) return agreements;
+    const normalized = term.toLocaleLowerCase();
+    const filtered: AgreementData = {};
+    for (const address in agreements) {
+      filtered[address] = agreements[address].filter((agreement) =>
+        JSON.stringify(agreement).toLocaleLowerCase().includes(normalized)
+      );
+    }
+    return filtered;
+  }, [agreements, term]);
 
   const onChangeHandler = (event: React.BaseSyntheticEvent) => {
     setTerm(event.target.value);
-
-    if (agreements && event.target.value.length > 1) {
-      const filteredData: AgreementData = {};
-      for (const address in agreements) {
-        filteredData[address] = agreements[address].filter((agreement) => {
-          return JSON.stringify(agreement).toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase());
-        });
-      }
-      setData(filteredData);
-    } else {
-      setData(agreements);
-    }
   };
 
   const onResetHandler = () => {
-    setData(agreements);
+    setTerm('');
   };
 
   const handleChangeLimit = (option: string) => {
