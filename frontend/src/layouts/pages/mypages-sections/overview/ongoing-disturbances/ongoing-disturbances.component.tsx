7499a583c91be208eba08e13fb6a42f79c3df9ed
aliceringstromsk <alice.ringstrom@sundsvall.se>
2026-05-22 08:57:54 +0200

    feat: hide overview disturbances

--- frontend/src/layouts/pages/mypages-sections/overview/ongoing-disturbances/ongoing-disturbances.component.tsx
+++ frontend/src/layouts/pages/mypages-sections/overview/ongoing-disturbances/ongoing-disturbances.component.tsx
@@ -1,20 +1,34 @@
 'use client';
 
 import { useTranslation } from 'react-i18next';
-import { Button } from '@sk-web-gui/react';
+import { Button, Spinner } from '@sk-web-gui/react';
 import { ArrowRight } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { getAdjustedPathname } from '@utils/representingModeRoute';
 import { useAppContext } from '@contexts/app.context';
-import DisturbancesList from '@components/disturbances-list/disturbances-list.component';
+import React from 'react';
+import { useApi } from '@services/api-service';
+import { Disturbance } from '@data-contracts/backend/data-contracts';
+import { DisturbanceListItem } from '@components/disturbances-list/disturbance-list-item/disturbance-list-item.component';
 
 export const OngoingDisturbances = () => {
   const { t } = useTranslation('disturbances');
   const router = useRouter();
   const { representingMode } = useAppContext();
   const adjustedPath = getAdjustedPathname('driftinformation', representingMode);
+  const statusParams = new URLSearchParams();
+  statusParams.append('status', 'OPEN,PLANNED');
+  const queryString = statusParams.toString();
 
-  return (
+  const { data, isFetching, isFetched } = useApi<Disturbance[]>({
+    url: `/disturbances?${queryString}`,
+    method: 'get',
+    queryKey: ['disturbances', 'OPEN,PLANNED'],
+  });
+
+  return isFetching ? (
+    <Spinner className="mx-auto" />
+  ) : isFetched && data?.length ? (
     <section className="mt-80" data-cy="overview-disturbances">
       <div className="flex sm:flex-row flex-col justify-between sm:items-center sm:mb-32 gap-32">
         <div>
@@ -32,7 +46,9 @@ export const OngoingDisturbances = () => {
         </Button>
       </div>
 
-      <DisturbancesList statuses={'OPEN,PLANNED'} />
+      {data?.map((disturbance) => (
+        <DisturbanceListItem key={disturbance.id} disturbance={disturbance} />
+      ))}
     </section>
-  );
+  ) : null;
 };
