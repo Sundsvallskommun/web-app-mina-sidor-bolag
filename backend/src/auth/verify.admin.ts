137a77d0fe3a3f90b26c68bd4cd6503b670e8e6d
aliceringstromsk <alice.ringstrom@sundsvall.se>
2026-06-22 13:17:25 +0200

    fix: auth service

--- backend/src/auth/verify.admin.ts
+++ backend/src/auth/verify.admin.ts
@@ -13,8 +13,6 @@ export const adminVerify = async (profile: Profile, done: VerifiedCallback) => {
     });
   }
 
-  console.log('>>>>> PROFILE ', profile);
-
   const username = profile.attributes?.['username'];
   const email = profile.attributes?.['email'];
   const groups = profile.attributes?.['groups'];
--- backend/src/services/authorization.service.ts
+++ backend/src/services/authorization.service.ts
@@ -6,15 +6,15 @@ export const defaultPermissions: () => Permissions = () => ({
   isImpersonatingUser: false,
 });
 
-export const getPermissionsByGroups = async (groups: string[] = []): Promise<Permissions> => {
+export const getPermissionsByGroups = async (groups: string): Promise<Permissions> => {
   const permissions: Permissions = defaultPermissions();
   if (!groups.length || !ADMIN_GROUP) return permissions;
 
-  const groupsList = groups.map((g: string) => g.toLowerCase());
+  const groupsList = (Array.isArray(groups) ? groups : groups.split(','))
+    .map(g => g.trim().toLowerCase())
+    .filter(Boolean);
   const adminGroup = ADMIN_GROUP.toLowerCase();
   permissions.canImpersonateUser = groupsList.includes(adminGroup);
-
-  console.log('>>>>> PERMISSIONS ', permissions);
 
   return permissions;
 };
